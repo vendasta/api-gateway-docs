26ccbd457f89ddc225ea9c14a077bebf4afd93b1
rrance@vendasta.com <rrance@vendasta.com>
2023-01-11 11:42:17 -0600

    Apply auto generated stoplight changes

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE1ODI5/spectral.js?branch_slug=master&token=2bae0758-cb25-4ec3-a196-7bc6e94a1cba";
+export default { extends: ruleset };

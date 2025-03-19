c7ed663f0aeb90fe41956a88caa74ff9585a2318
varsnaga <vnagasubramanian@vendasta.com>
2025-03-19 17:46:54 +0530

    Script for Merging OpenAPI specs

--- examples/go/gateway-docs/go.mod
+++ examples/go/gateway-docs/go.mod
@@ -2,16 +2,38 @@ module gateway-docs
 
 go 1.23.6
 
-require github.com/getkin/kin-openapi v0.129.0
+require (
+	github.com/getkin/kin-openapi v0.129.0
+	github.com/weblfe/go-swagger-merger v0.1.8
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/oasdiff/yaml v0.0.0-20241210131133-6b86fb107d80 // indirect
 	github.com/oasdiff/yaml3 v0.0.0-20241210130736-a94c01f36349 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.15.0 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
---
+++ examples/go/gateway-docs/mergeOpenAPISpecs.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+const inputDir1 = "./vendastaapisgen1" // Directory containing Swagger JSON files
+
+// const outputFile1 = "./openapi/grpc/grpc_api.swagger.json"
+const outputFile1 = "../../../openapi/grpc/grpc_api.swagger.json"
+
+func main() {
+	inputDir := flag.String("input", "", "Directory containing Swagger JSON files")
+	// Validate required arguments
+	if *inputDir == "" {
+		//log.Fatal("Usage: go run main.go --input <input_dir> ")
+	}
+	fmt.Println("Starting Swagger JSON Merge Process...")
+
+	// Collect all Swagger JSON files
+	swaggerFiles, err := getSwaggerFiles1(inputDir1)
+	if err != nil {
+		log.Fatalf("Failed to collect Swagger files: %v", err)
+	}
+
+	fmt.Printf("Found %d Swagger files\n", len(swaggerFiles))
+
+	// Merge the files
+	mergedSwagger, err := mergeSwaggerFiles(swaggerFiles)
+	if err != nil {
+		log.Fatalf("Failed to merge Swagger files: %v", err)
+	}
+
+	// Write the merged file
+	if err := ioutil.WriteFile(outputFile1, mergedSwagger, 0644); err != nil {
+		log.Fatalf("Error writing merged Swagger file: %v", err)
+	}
+
+	fmt.Println("Successfully merged Swagger files into:", outputFile1)
+}
+
+// getSwaggerFiles scans the input directory for all `api.swagger.json` files
+func getSwaggerFiles1(dir string) ([]string, error) {
+	var files []string
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && info.Name() == "api.swagger.json" {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	return files, err
+}
+
+// mergeSwaggerFiles reads and merges multiple JSON Swagger files
+func mergeSwaggerFiles(files []string) ([]byte, error) {
+	mergedData := make(map[string]interface{})
+	mergedData["swagger"] = "2.0"
+
+	for _, file := range files {
+		fmt.Println("Processing:", file)
+
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			log.Printf("Skipping %s (failed to read): %v\n", file, err)
+			continue
+		}
+
+		var swagger map[string]interface{}
+		if err := json.Unmarshal(data, &swagger); err != nil {
+			log.Printf("Skipping %s (invalid JSON): %v\n", file, err)
+			continue
+		}
+
+		// Merge top-level fields
+		mergeField("paths", swagger, mergedData)
+		mergeField("definitions", swagger, mergedData)
+		mergeField("components", swagger, mergedData)
+		mergeField("info", swagger, mergedData) // API Metadata
+
+		// Ensure 'info' exists with a valid title
+		if _, exists := mergedData["info"]; !exists {
+			mergedData["info"] = map[string]interface{}{
+				"title":   "Merged API",
+				"version": "1.0.0",
+			}
+		}
+	}
+
+	// Convert merged data to JSON
+	mergedJSON, err := json.MarshalIndent(mergedData, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+
+	return mergedJSON, nil
+}
+
+// mergeField merges specific fields like "paths" or "definitions"
+func mergeField(field string, source, target map[string]interface{}) {
+	if sourceField, ok := source[field].(map[string]interface{}); ok {
+		if _, exists := target[field]; !exists {
+			target[field] = make(map[string]interface{})
+		}
+		for k, v := range sourceField {
+			target[field].(map[string]interface{})[k] = v
+		}
+	}
+}
